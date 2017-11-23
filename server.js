3986c948c74dc7d63a209b0ceeb5d9fe31b99995
Chris Northwood <chris.northwood@hotmail.com>
2017-11-23 19:38:36 +0000

    Added app for list making, altered landing page to include a 'login' and 'register' button which will handle the data entry

--- server.js
+++ server.js
@@ -43,45 +43,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-//Register Page
-app.get("/register", (req, res) =>{
-  res.render("reg_index");
-});
-
-app.post("/register", (req, res) => {
-  let userName = req.body.username;
-  let userPassword = req.body.password;
-  // if (!userName || !userPassword) {
-  //   res.status(400).send("400 - Registration not completed");
-  // } else if (emailChecker(myEmail)) {
-  //   res.status(400).send("400 - User e-mail exists already");
-  // } else {
-  //   let user = {
-  //     id: userName,
-  //     password: userPassword
-  //   };
-    res.redirect('/to-do')
-});
-
-
-
-//Login Page
-app.get("/login", (req, res) => {
-  res.render("login_index")
-});
-
-app.post("/login", (req, res) => {
-  let userName = req.body.username;
-  let userPass = req.body.password;
-  if (userName === '' || userPass === '') {
-    res.send(403);
-    return;
-  } else {
-    res.redirect("/to-do")
-  }
-});
-
-
 //To-Do Main PAGE
 app.get("/to-do", (req, res) =>{
   res.render("test_index")
@@ -90,12 +51,6 @@ app.get("/to-do", (req, res) =>{
 //Profile Page
 app.get("/profile", (req, res) =>{
   res.render("profile_index")
-});
-
-
-//Edit Page
-app.get("/edit/", (req, res) =>{
-  res.render("index")
 });
 
 
